4242f758bfed84425752d9bb4634033ac003ad56
Vishal Yadav <ydvishal89@gmail.com>
2026-07-11 11:47:20 +0530

    Refactor App component to use functional component syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import react from 'react'
+
+function app(){
+  return( 
+      <div id="main">
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+      </div>
+  );
+}
+export default app;
 
